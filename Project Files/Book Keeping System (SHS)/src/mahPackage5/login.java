93e5eb50343c4dd6c32e308115490650c957e49e
KramBangcaya <markbangcaya99@gmail.com>
2021-09-16 17:30:58 +0800

    Update login.java

--- Project Files/Book Keeping System (SHS)/src/mahPackage5/login.java
+++ Project Files/Book Keeping System (SHS)/src/mahPackage5/login.java
@@ -363,7 +363,7 @@ public class login extends javax.swing.JFrame implements java.lang.Runnable {
         String password = String.valueOf(pass.getPassword());
         
         //convert the password to md5 hash
-        int result = my.login(userName, my.convertMd5(password), new int [] {1,4,5});
+        int result = my.login(userName, password, new int [] {1,4,5});
 
         switch(result){
             case 0:{
